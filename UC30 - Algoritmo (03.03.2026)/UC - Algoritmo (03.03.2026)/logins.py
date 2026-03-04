b4e17f5ab6c1537bce2b0a6a898c9ba5feb88c31
helosvflx <heloisasevf@gmail.com>
2026-03-04 09:55:28 -0300

    Update logins.py

--- UC30 - Algoritmo (03.03.2026)/UC - Algoritmo (03.03.2026)/logins.py
+++ UC30 - Algoritmo (03.03.2026)/UC - Algoritmo (03.03.2026)/logins.py
@@ -1,21 +1,7 @@
-login1 = "procopio"
-senha1 = 12345
-login2 = "paiva"
-senha2 = 54321
+usuario = input("Digite o seu login: ")
+senha = input("Digite a senha: ")
 
-login1 = input("Digite o seu primeiro login: ")
-senha1 = input("Digite a senha: ")
-
-if login1 == "procopio" and senha1 == 12345:
-    print("Seja bem vindo!")
-else:
-    print("Usuário e senha não conferem.")
-
-
-login2 = input("Digite o seu segundo login: ")
-senha2 = input("Digite a senha: ")
-
-if login2 == "paiva" and senha2 == 54321:
+if (usuario == "procopio" and senha == "12345") or (usuario == "paiva" and senha == "54321"):
     print("Seja bem vindo!")
 else:
     print("Usuário e senha não conferem.")
